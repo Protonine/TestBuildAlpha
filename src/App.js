fa32b56a90728b38103c291a4961a2c1497bd12a
Protonine <78059436+Protonine@users.noreply.github.com>
2021-02-02 00:35:52 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App" class="Center">
+      <h1>Testing</h1>
+    </div>
+  );
+}
